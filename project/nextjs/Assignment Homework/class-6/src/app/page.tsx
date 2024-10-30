51ae80338d914f4a64aa98db3dff40099106c40e
mfsrajput <mfs.khi@gmail.com>
2024-10-31 04:59:33 +0500

    add assigment 6

---
+++ project/nextjs/Assignment Homework/class-6/src/app/page.tsx
@@ -0,0 +1,20 @@
+import NavBar from "@/components/navBar";
+import HeroSection from "@/components/heroSection";
+import FeaturedCars from  "@/components/FeaturedCars";
+import Footer  from "@/components/Footer";
+
+
+export default function Home() {
+  return (
+   <div>
+
+    <NavBar />
+    <HeroSection />
+    <FeaturedCars />
+    <Footer />
+
+
+
+   </div>
+  );
+}
