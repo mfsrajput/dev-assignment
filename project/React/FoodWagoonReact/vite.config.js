2d77416eb56b974d96c9d7ed7dcf23f5d8e77b8c
mfsrajput <mfs.khi@gmail.com>
2024-11-25 23:37:38 +0500

    add foodwagon assignment

--- project/React/FoodWagoonReact/vite.config.js
+++ project/React/FoodWagoonReact/vite.config.js
@@ -4,7 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src", // Set src as the root
-  build: {
-    outDir: "../FoodWagoonReact/src/dist", // Ensure the dist folder is created outside src
-  },
+ 
 });
