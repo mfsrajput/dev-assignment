7ce90fe407027c33cd58165627a6a2ae87b2a882
mfsrajput <mfs.khi@gmail.com>
2024-11-25 22:59:31 +0500

    add foodwagon assignment

--- project/React/FoodWagoonReact/src/index.js
+++ project/React/FoodWagoonReact/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import AOS from 'aos';
+import 'aos/dist/aos.css';  // If you're using CSS as well
 import App from "./App.jsx";
+import { useEffect } from 'react';
+
+    useEffect(() => {
+        AOS.init({
+          duration: 1000,
+        });
+      }, []);
 const container = document.getElementById("root");
 const root = createRoot(container);
 // root.render(React.createElement("h2", {}, "We are learning React"));
--- project/React/FoodWagoonReact/vite.config.js
+++ project/React/FoodWagoonReact/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
- 
+
 export default defineConfig({
   plugins: [react()],
-  root: "src",
+  root: "src", // Set src as the root
+  build: {
+    outDir: "../FoodWagoonReact/src/dist", // Ensure the dist folder is created outside src
+  },
 });
