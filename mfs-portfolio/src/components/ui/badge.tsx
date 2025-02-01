27695f8bf45dadc87193fd8a30dee3505731ad03
mfsrajput <mfs.khi@gmail.com>
2025-02-01 13:45:54 +0500

    remove one assignment in this repo

--- mfs-portfolio/src/components/ui/badge.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils"
-
-type BadgeProps = React.HTMLAttributes<HTMLSpanElement>
-
-export function Badge({ className, ...props }: BadgeProps) {
-  return (
-    <span className={cn("px-2 py-1 text-sm font-medium bg-primary text-white rounded-md", className)} {...props} />
-  )
-}
